8b29bf7a2f27775d909459fe26e656be4434c9f6
Tetiana <rudenkotatyana19@gmail.com>
2021-06-10 14:54:26 +0300

    refactoring code with hooks

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Statistics from './components/Statistics';
 import FeedbackOptions from './components/FeedbackOptions';
 import Section from './components/Section';
 
-class App extends Component {
-  state = {
+export default function App() {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleIncrement = event => {
+  const handleIncrement = event => {
     const name = event.target.name;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+
+    setState(prevState => ({ ...state, [name]: prevState[name] + 1 }));
   };
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
     const totalFeedback = good + neutral + bad;
 
     return totalFeedback;
-  }
+  };
 
-  countPositiveFeedbackPercentage() {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((state.good * 100) / countTotalFeedback());
+  };
 
-  render() {
-    return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={this.state}
-          onLeaveFeedback={this.handleIncrement}
-        />
-        <Statistics
-          title="Statistics"
-          options={this.state}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        />
-      </Section>
-    );
-  }
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions options={state} onLeaveFeedback={handleIncrement} />
+      <Statistics
+        title="Statistics"
+        options={state}
+        total={countTotalFeedback()}
+        positivePercentage={countPositiveFeedbackPercentage()}
+      />
+    </Section>
+  );
 }
-
-export default App;
